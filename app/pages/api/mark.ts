f53033fd72520394e0acbafca53726162d117a0d
alexparshh <alexparshh@gmail.com>
2023-07-18 09:03:42 -0700

    boilerplate

---
+++ app/pages/api/mark.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type ResponseData = {
+  message: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  if (req.method === 'POST') {
+    handlePostRequest(req, res)
+  } else {
+    res.status(400).json({ message: 'Invalid request method' })
+  }
+}
+
+function handlePostRequest(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  res.status(200).json({ message: 'Post request successful' })
+}
---
+++ app/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const IndexPage: React.FC = () => {
+  return <div>Test</div>;
+};
+
+export default IndexPage;
